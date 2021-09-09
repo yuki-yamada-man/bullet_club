3f9671b7c708b72f45729fb19a1e7e900b7a81fe
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-09-09 12:45:52 +0000

    make_user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = Users.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  def quit
+    @user = User.find(params[:id])
+  end
+  
+  def out
+    @user = User.find(params[:id])
+    if @user.update(is_enabled: false)
+      sign_out current_user
+    end
+    redirect_to root_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:image,:content,:indroduction)
+  end
 end
