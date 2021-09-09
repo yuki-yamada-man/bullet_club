4bc7bb60a01688edf8b26edabf1786b7f05812df
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-09-09 12:05:50 +0000

    make_controllers

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,27 +1,46 @@
 class FoodsController < ApplicationController
   
   def new
+    @food = Food.new
   end
   
   def create
+    @food = Food.new(food_params)
+    @food.save
+    redirect_to finish_food_path
   end
   
   def finish
   end
   
   def index
+    @foods = Food.all
   end
   
   def show
+    @food = Food.find(params[:id])
   end
   
   def edit
+    @food = Food.find(params[:id])
   end
   
   def update
+    @find = Food.find(params[:id])
+    @find.update(food_params)
+    redirect_to food_path(@food.id)
   end
   
   def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+  
+  private
+  
+  def food_params
+    params.require(:food).permit(:time,:eat)
   end
   
 end
--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,27 +1,46 @@
 class TrainingsController < ApplicationController
   
   def new
+    @trainig = Trainig.new
   end
   
   def create
+    @trainig = Trainig.new(trainig_params)
+    @trainig.save
+    redirect_to finish_trainig_path
   end
   
   def finish
   end
   
   def index
+    @trainigs = Trainig.all
   end
   
   def show
+    @trainig = Trainig.find(params[:id])
   end
   
   def edit
+    @trainig = Trainig.find(params[:id])
   end
   
   def update
+    @trainig = Trainig.find(params[:id])
+    @trainig.update(trainig_params)
+    redirect_to trainig_path(@trainig.id)
   end
   
   def destroy
+    @trainig = Trainig.find(params[:id])
+    @trainig.destroy
+    redirect_to trainigs_path
+  end
+  
+  private
+  
+  def trainig_params
+    params.require(:trainig).permit(:menu,:kg,:count,:set)
   end
   
 end
